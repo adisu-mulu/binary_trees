ab50db36ba2357a58a42abb53f0f072b8edc32f5
adisu-mulu <kulhabesh31@gmail.com>
2024-06-05 16:13:07 +0300

    modified 1-binary

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,9 +9,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
 
-	if (newnode == NULL)
-		return (NULL);
 	if (parent == NULL)
+		return (NULL);
+	if (newnode == NULL)
 		return (NULL);
 	newnode->n = value;
 	newnode->left = NULL;
