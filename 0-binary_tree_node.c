42ffdd498384a52b665c2403d368595e2ef07d8c
adisu-mulu <kulhabesh31@gmail.com>
2024-06-05 09:55:52 +0300

    adding documentation to 0-binary

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_node - function to create a binary tree node
+ * @parent: parent node of the node to be created
+ * @value: value to be inserted in the node
+ * Return: the address of the created node
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
