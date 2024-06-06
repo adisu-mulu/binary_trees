956f28b9b61dfff78137d22688b1f7d7856248fe
adisu-mulu <kulhabesh31@gmail.com>
2024-06-06 16:03:40 +0300

    added height calculation with in the file

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,23 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_height - function to measure size
+ * @tree: pointer to the node of the tree
+ * Return: size of the height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lh, rh;
+
+	if (tree == NULL)
+		return (0);
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
+	if (lh > rh)
+		return (lh + 1);
+	else
+		return (rh + 1);
+}
+/**
  * binary_tree_balance - function to measure the balance factor
  * @tree: pointer to the node
  * Return: balance factor
