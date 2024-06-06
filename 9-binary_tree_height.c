b21719258a6c232ed8c59518637747db65f058dd
adisu-mulu <kulhabesh31@gmail.com>
2024-06-06 10:01:57 +0300

    adding 9-binary height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 	lh = binary_tree_height(tree->left);
 	rh = binary_tree_height(tree->right);
-	return (1 + ((lh > rh) ? lh : rh));
+	if (lh > rh)
+		return (lh + 1);
+	else
+		return (rh + 1);
 }
